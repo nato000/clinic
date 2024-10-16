99e320a4fc47a276e7840801f678b5994f8898a6
nato000 <vganja2@gmail.com>
2024-10-16 22:16:59 +0300

    add header

--- src/app/layout/layout.jsx
+++ src/app/layout/layout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../shared/widgets/header'
 import { Footer } from '../../shared/widgets/footer'
+import './layout.css'
 
 export const Layout = () => {
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      <div className="layout-wrapper">
+        <Header />
+        <div className="main-wrapper">
+          <Outlet />
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/shared/widgets/header/ui/dropdown.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './dropdown.css'
+
+const DropdownMenu = () => {
+  return (
+    <div className="dropdown-menu">
+      <ul className="dropdown-menu-list">
+        <li className="dropdown-menu-list-item">Menu 1</li>
+        <li className="dropdown-menu-list-item">Menu 2</li>
+        <li className="dropdown-menu-list-item">Menu 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default DropdownMenu
