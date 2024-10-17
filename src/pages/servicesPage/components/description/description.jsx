18b7d0522140ef6005dd98a7d849e209f755285f
edmishchenko <edmishchenko86@gmail.com>
2024-10-18 00:17:12 +0200

    Update

---
+++ src/pages/servicesPage/components/description/description.jsx
@@ -0,0 +1,27 @@
+import "./style.css";
+const Description = ({title, description, symptoms}) => {
+  return (
+    <section className="services-description-section">
+      <div className="container">
+        <div className="description-wrapper">
+          <div className='description-column'>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <div className="symptoms-container">
+              {
+                symptoms.map((item, index) => (
+                  <div className='symptom-item' key={index}>
+                    <span className='dot'></span>
+                    <p>{item}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Description;
